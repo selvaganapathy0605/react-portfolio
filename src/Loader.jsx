a6fe22a841a0f6621dcee3b830a07d1ca04c0f3c
selvaganapathy D <140634928+selvaganapathy0605@users.noreply.github.com>
2025-03-13 19:29:48 +0300

    first commit

---
+++ src/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import loader from './assets/load-gif.gif'
+
+const Loader = () => {
+  return (
+    <div id='preloader'>
+        <img src={loader} alt="" />
+    </div>
+  )
+}
+
+export default Loader
