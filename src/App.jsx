c5d93e06fbff31bc8d743f4b7cf7247008d7ee0d
selvaganapathy D <140634928+selvaganapathy0605@users.noreply.github.com>
2025-03-13 22:00:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React ,{ useState, useEffect } from 'react'
-
+import React, { useState, useEffect } from 'react'
 import Header from './Header'
 import About from './About'
 import Service from './Service'
@@ -7,21 +6,31 @@ import Resume from './Resume'
 import Banner from './Banner'
 import Blog from './Blog'
 import Footer from './Footer'
-
+import Loader from './Loader'
 
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   return (
     <>
-      <Header />
-      <About />
-      <Service />
-      <Resume />
-      <Banner />
-      <Blog />
-      <Footer />
+      {loading ? <Loader /> : <>
+        <Header />
+        <About />
+        <Service />
+        <Resume />
+        <Banner />
+        <Blog />
+        <Footer />
+      </>}
     </>
   )
 }
-
 export default App
